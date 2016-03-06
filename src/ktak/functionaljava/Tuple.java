b2cde15932e654ca7c9ede4781c596d1ab523f84
KMT <ramune07@gmail.com>
2016-03-05 16:42:28 -0800

    add Tuple type

---
+++ src/ktak/functionaljava/Tuple.java
@@ -0,0 +1,17 @@
+package ktak.functionaljava;
+
+public class Tuple<A,B> {
+    
+    public final A left;
+    public final B right;
+    
+    public Tuple(A left, B right) {
+        this.left = left;
+        this.right = right;
+    }
+    
+    public static <A,B> Tuple<A,B> create(A left, B right) {
+        return new Tuple<A,B>(left, right);
+    }
+    
+}
