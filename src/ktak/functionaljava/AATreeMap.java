81826ee0799342134c450f12347cdffc6c3f401c
KMT <ramune07@gmail.com>
2016-03-06 16:45:00 -0800

    add map implementation

---
+++ src/ktak/functionaljava/AATreeMap.java
@@ -0,0 +1,70 @@
+package ktak.functionaljava;
+
+import java.util.Comparator;
+
+public class AATreeMap<K,V> {
+    
+    private final AATree<Tuple<K,V>> tree;
+    
+    public AATreeMap(final Comparator<K> comparator) {
+        
+        this.tree = AATree.emptyTree(new Comparator<Tuple<K,V>>() {
+            
+            @Override
+            public int compare(Tuple<K,V> o1, Tuple<K,V> o2) {
+                return comparator.compare(o1.left, o2.left);
+            }
+            
+        });
+        
+    }
+    
+    private AATreeMap(AATree<Tuple<K,V>> tree) { this.tree = tree; }
+    
+    public static <K,V> AATreeMap<K,V> emptyMap(Comparator<K> comparator) {
+        return new AATreeMap<K,V>(comparator);
+    }
+    
+    public AATreeMap<K,V> insert(K key, V value) {
+        
+        Tuple<K,V> treeValue = Tuple.create(key, value);
+        
+        // the comparator only looks at the key
+        if (tree.contains(treeValue))
+            return new AATreeMap<K,V>(tree.remove(treeValue).insert(treeValue));
+        
+        return new AATreeMap<K,V>(tree.insert(treeValue));
+        
+    }
+    
+    public Boolean containsKey(K key) {
+        return tree.contains(Tuple.create(key, (V)null));
+    }
+    
+    public Option<V> get(K key) {
+        
+        return tree.get(Tuple.create(key, (V)null)).visit(new Option.Visitor<Option<V>,Tuple<K,V>>() {
+            
+            @Override
+            public Option<V> visitNone() {
+                return Option.none();
+            }
+            
+            @Override
+            public Option<V> visitSome(Tuple<K, V> value) {
+                return Option.some(value.right);
+            }
+            
+        });
+        
+    }
+    
+    public AATreeMap<K,V> remove(K key) {
+        return new AATreeMap<K,V>(tree.remove(Tuple.create(key, (V)null)));
+    }
+    
+    public Long size() {
+        return tree.size();
+    }
+    
+}
---
+++ test/ktak/functionaljava/AATreeMapTest.java
@@ -0,0 +1,129 @@
+package ktak.functionaljava;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Random;
+
+import org.junit.Test;
+import org.junit.Assert;
+
+public class AATreeMapTest {
+    
+private static final Comparator<Integer> cmp = new Comparator<Integer>() {
+        
+        @Override
+        public int compare(Integer arg0, Integer arg1) {
+            return arg0.compareTo(arg1);
+        }
+        
+    };
+    
+    @Test
+    public void testSize() {
+        
+        AATreeMap<Integer,Integer> map = AATreeMap.emptyMap(cmp);
+        
+        List<Integer> list = consecutiveIntegerList(100);
+        Long mapSize = 0L;
+        
+        for (Integer unique : list) {
+            map = map.insert(unique, unique);
+            mapSize++;
+            Assert.assertEquals(mapSize, map.size());
+        }
+        
+        for (Integer unique : list) {
+            map = map.insert(unique, unique);
+            Assert.assertEquals(mapSize, map.size());
+        }
+        
+    }
+    
+    @Test
+    public void testContains() {
+        
+        AATreeMap<Integer,Integer> map = AATreeMap.emptyMap(cmp);
+        
+        List<Integer> randomList = randomIntegerList(1000, 900);
+        for (Integer random : randomList) {
+            map = map.insert(random, random);
+            Assert.assertTrue(map.containsKey(random));
+        }
+        
+        for (Integer random : randomList) {
+            Assert.assertTrue(map.containsKey(random));
+        }
+        
+    }
+    
+    @Test
+    public void testGet() {
+        
+        AATreeMap<Integer,Integer> map = AATreeMap.emptyMap(cmp);
+        
+        List<Integer> list = consecutiveIntegerList(1000);
+        List<Integer> randomList = randomIntegerList(1000, 1000);
+        for (int i=0; i < list.size(); i++) {
+            map = map.insert(list.get(i), randomList.get(i));
+        }
+        
+        for (int i=0; i < list.size(); i++) {
+            final Integer random = randomList.get(i);
+            Assert.assertTrue(map.get(list.get(i)).visit(
+                    new Option.Visitor<Boolean,Integer>() {
+                        
+                        @Override
+                        public Boolean visitNone() {
+                            return false;
+                        }
+                        
+                        @Override
+                        public Boolean visitSome(Integer value) {
+                            return value.equals(random);
+                        }
+                        
+                    }));
+        }
+        
+    }
+    
+    @Test
+    public void testRemove() {
+        
+        AATreeMap<Integer,Integer> map = AATreeMap.emptyMap(cmp);
+        
+        List<Integer> list = consecutiveIntegerList(1000);
+        for (Integer unique : list) {
+            map = map.insert(unique, unique);
+        }
+        
+        for (Integer unique : list) {
+            map = map.remove(unique);
+            Assert.assertFalse(map.containsKey(unique));
+        }
+        
+    }
+    
+    private List<Integer> consecutiveIntegerList(int size) {
+        
+        List<Integer> list = new ArrayList<Integer>();
+        for (int i=0; i < size; i++) {
+            list.add(i);
+        }
+        return list;
+        
+    }
+    
+    private List<Integer> randomIntegerList(int size, int modulo) {
+        
+        List<Integer> randomList = new ArrayList<Integer>();
+        Random rand = new Random();
+        for (int i=0; i < size; i++) {
+            randomList.add(rand.nextInt() % modulo);
+        }
+        return randomList;
+        
+    }
+    
+}
